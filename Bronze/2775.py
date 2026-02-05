e9fcd3a67fb67c5009e05f02143eeea3d3d95ef9
uyeong <uyeong1575@gmail.com>
2026-02-05 22:29:19 +0900

    Solve: 2775 부녀회장이 될테야

---
+++ Bronze/2775.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+
+    arr = [i for i in range(1, n+1)]
+
+    for _ in range(k):
+        for j in range(1, n):
+            arr[j] += arr[j-1]
+
+    print(arr[-1])
