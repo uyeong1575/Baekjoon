3737d7b543e70437e9c35d716df5b9fb6e665834
uyeong <uyeong1575@gmail.com>
2026-03-19 19:27:33 +0900

    Solve: 1929 소수 구하(math, sieve)

---
+++ 1929.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+is_prime = [True] * (N + 1)
+is_prime[0] = False
+is_prime[1] = False
+
+# 제곱근까지
+for i in range(2, int(N**0.5) + 1):
+    if is_prime[i]:
+        for j in range(i * i, N + 1, i):
+            is_prime[j] = False
+
+for i in range(M, N + 1):
+    if is_prime[i]:
+        print(i)
+
+        
+
